a908a79b61935a6bddabf616ce01b066addcf00c
maruf <maruf12@student.sust.edu>
2023-10-22 12:58:55 +0600

    server menu wip

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
+import ServerHeader from "./server-header";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -33,13 +34,28 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
     },
   });
 
-  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
   const audioChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.AUDIO
   );
   const videoChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.VIDEO
-  ); 
+  );
 
-  return <div></div>;
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile.id
+  );
+
+  if (!server) return redirect("/");
+
+  const role = server.members.find((member) => member.profileId === profile.id)?.role
+
+  return <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]">
+    <ServerHeader
+    server={server}
+    role={role}
+    />
+  </div>;
 }
