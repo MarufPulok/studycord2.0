94aba80ffbc484650e195c9b5a5a7f74a24fb4c9
maruf <maruf12@student.sust.edu>
2023-10-22 12:58:43 +0600

    server menu wip

---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,45 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface ServerSidebarProps {
+  serverId: string;
+}
+
+export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
+  const profile = await currentProfile();
+  if (!profile) return redirect("/");
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videoChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  ); 
+
+  return <div></div>;
+}
