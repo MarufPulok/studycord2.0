922aa39fe5579648b7b06b01a8e3f9fac34da2ca
maruf <maruf12@student.sust.edu>
2023-10-23 23:39:11 +0600

    kick members

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -54,3 +54,51 @@ export async function PATCH(req: Request, { params }: { params: { memberId: stri
         return new Response("Internal server error", { status: 500 })
     }
 }
+
+
+export async function DELETE(req: Request, { params }: { params: { memberId: string } }) {
+    try {
+        const profile = await currentProfile()
+        const { searchParams } = new URL(req.url)
+
+        const serverId = searchParams.get("serverId")
+
+        if (!profile) return new NextResponse("Unauthorized", { status: 401 })
+
+        if (!serverId) return new NextResponse("Bad request", { status: 400 })
+
+        if (!params.memberId) return new NextResponse("Bad request", { status: 400 })
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                profileId: profile.id
+            },
+            data: {
+                members: {
+                    deleteMany: {
+                        id: params.memberId,
+                        profileId: {
+                            not: profile.id
+                        }
+                    }
+                }
+            },
+            include: {
+                members: {
+                    include: {
+                        profile: true
+                    },
+                    orderBy: {
+                        role: "asc"
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(server)
+    } catch (error) {
+        console.log("[MEMBERS_ID_DELETE]", error)
+        return new NextResponse("Internal server error", { status: 500})
+    }
+}
