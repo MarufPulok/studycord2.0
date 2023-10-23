a751bc8bb5807e746a586e1f06a8fd892821edbe
maruf <maruf12@student.sust.edu>
2023-10-23 16:00:31 +0600

    invite complete

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,52 +1,51 @@
-import { currentProfile } from '@/lib/current-profile'
-import { db } from '@/lib/db'
-import { redirectToSignIn } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
 interface InviteCodePageProps {
-    params: {
-        inviteCode: string
-    }
+  params: {
+    inviteCode: string;
+  };
 }
 
 export default async function InviteCodePage({ params }: InviteCodePageProps) {
-    const profile =  await currentProfile()
+  const profile = await currentProfile();
 
-    if (!profile) return redirectToSignIn()
+  if (!profile) return redirectToSignIn();
 
-    if(!params.inviteCode) return redirect('/')
+  if (!params.inviteCode) return redirect("/");
 
-    const existingServer = await db.server.findFirst({
-        where: {
-            inviteCode: params.inviteCode,
-            members: {
-                some: {
-                    profileId: profile.id
-                }
-            }
-        }
-    })
-
-    if (existingServer) return redirect(`/servers/${existingServer.id}`)
-
-    const server = await db.server.update({
-        where: {
-            inviteCode: params.inviteCode
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
         },
-        data: {
-            members: {
-                create: [
-                    {
-                        profileId: profile.id 
-                    }
-                ]
-            }
-        }
-    })
-   return (
-    <div>
-      invite code
-    </div>
-  )
+      },
+    },
+  });
+
+  if (existingServer) return redirect(`/servers/${existingServer.id}`);
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          },
+        ],
+      },
+    },
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+
+  return null;
 }
