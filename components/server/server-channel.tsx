fc8de023e58101883fdea3aa92b1b54fbb1e065c
maruf <maruf12@student.sust.edu>
2023-10-26 00:42:43 +0600

    edit channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -32,7 +32,7 @@ export default function ServerChannel({
   const Icon = iconMap[channel.type];
   return (
     <button
-      onClick={() => {}}
+      onClick={() => onOpen("editChannel", {server, channel})}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
