ec161e392a7e10accec3314925f3855e970996b9
maruf <maruf12@student.sust.edu>
2023-10-25 18:18:57 +0600

    channel type fixes

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -35,6 +35,8 @@ import {
   SelectValue,
 } from "../ui/select";
 import qs from "query-string";
+import { useEffect } from "react";
+import { channel } from "diagnostics_channel";
 
 const formSchema = z.object({
   name: z
@@ -49,19 +51,25 @@ const formSchema = z.object({
 });
 
 export default function CreateChannelModal() {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
   const params = useParams()
 
   const isModalOpen = isOpen && type === "createChannel";
+  const {channelType} = data
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+useEffect(() => {
+  if (channelType) form.setValue("type", channelType)
+  else form.setValue("type", ChannelType.TEXT)
+}, [channelType, form])
 
   const isLoading = form.formState.isSubmitting;
 
