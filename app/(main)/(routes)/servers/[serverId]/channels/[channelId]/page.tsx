5cac3000c32d3c7bc89c75656f65c39c24abe6c6
maruf <maruf12@student.sust.edu>
2023-10-27 00:38:34 +0600

    message send works?

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -30,13 +31,25 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
     },
   });
 
-  if (!member || !channel) redirect("/")
+  if (!member || !channel) redirect("/");
 
-  return <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-    <ChatHeader
-      name={channel.name}
-      serverId={channel.serverId}
-      type="channel"
-    />
-  </div>;
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      <div className="flex-1">future messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
 }
